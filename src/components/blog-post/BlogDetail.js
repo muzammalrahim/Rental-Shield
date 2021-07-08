125c8600f47dc7e72bbbd4ca049b643436d51697
Muzammal Rahim <creative.joomdev@gmail.com>
2021-07-08 18:13:45 +0500

    fixes

--- src/components/blog-post/BlogDetail.js
+++ src/components/blog-post/BlogDetail.js
@@ -5,6 +5,10 @@ import React, { Component } from 'react'
 // Images
 import BlogImg from '../../assets/images/blog-detail-img.png';
 
+// Component
+
+import SocialLinks from '../blog-post/SocialLinks';
+
 
 export default class BlogDetail extends Component {
     render() {
@@ -27,36 +31,36 @@ export default class BlogDetail extends Component {
                         {/* Main Heading */}
 
                         {/* Img Brief Description */}
-                        <p className="text-3xl font-body font-bold text-gray-500 text-left leading-9 pt-4">
+                        <p className="text-3xl font-body font-bold text-gray-500 text-left leading-9 pt-4 pb-3">
                             Create a blog post subtitle that summarizes your post in a few short,
                             punchy sentences and entices your audience to continue reading.
                         </p>
                         {/* Img Brief Description */}
 
                         {/* Blog Image */}
-                        <img src={BlogImg } alt="img"/>
+                        <img className="w-full" src={BlogImg } alt="img"/>
                         {/* Blog Image */}
 
-                        <p className="text-gray-500 font-body text-left font-normal text-2lx leading-8">
+                        <p className="text-gray-500 font-body text-left font-normal text-2lx leading-8 pt-5">
                             Welcome to your blog post. Use this space to connect with your readers 
                             and potential customers in a way that’s current and interesting. Think 
                             of it as an ongoing conversation where you can share updates about business, 
                             trends, news, and more. 
                         </p>
                         
-                        <p className="text-green-500 font-body font-normal text-left text-3xl leading-8">
+                        <p className="text-green-500 font-body font-normal text-left text-3xl leading-8 pt-4">
                             Do you have a design in mind for your blog? Whether you prefer a trendy 
                             postcard look or you’re going for a more editorial style blog - there’s a 
                             stunning layout for everyone.
                         </p>
 
-                        <p className="text-gray-500 font-body text-left font-normal text-2lx leading-8">
+                        <p className="text-gray-500 font-body text-left font-normal text-2lx leading-8 pt-4">
                             You’ll be posting loads of engaging content, so be sure to keep your blog 
                             organized with Categories that also allow visitors to explore more of what 
                             interests them.
                         </p>
 
-                        <p className="text-gray-500 font-body text-left font-bold text-2lx leading-8"> 
+                        <p className="text-gray-500 font-body text-left font-bold text-2lx leading-8 py-4"> 
                             Create Relevant Content
                         </p>
 
@@ -73,8 +77,10 @@ export default class BlogDetail extends Component {
                             Blogging gives your site a voice, so let your business’ personality shine 
                             through. Choose a great image to feature in your post or add a video for 
                             extra engagement. Are you ready to get started? Simply create a new post now.
+                            
                         </p>
                     </div>
+                    <SocialLinks/>
                 
             </div>
         )
