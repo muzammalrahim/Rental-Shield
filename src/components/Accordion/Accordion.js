0b5c39d005ef78b5c5dea69ccd4f2e9e541f4f3f
Muzammal Rahim <creative.joomdev@gmail.com>
2021-07-15 15:50:45 +0500

    fix home page accordions

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,22 +1,21 @@
 import React, { useState } from "react";
 import { Row, Col } from "react-bootstrap";
-import Collapse from "react-bootstrap/Collapse";
-
+import useCollapse from "react-collapsed";
 
 const Accordion = ({ title, content }) => {
-  const [isActive, setIsActive] = useState(false);
+  const [isExpanded, setExpanded] = useState(false);
+  const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded });
 
   return (
     <div className="accordion-item">
       <Row>
         <Col md="12" lg="12">
           <div
-            style={{borderRadius:"10px"}}
+            style={{ borderRadius: "10px" }}
             className=" border-gray-500 shadow m-2 pt-4 pb-16 pl-4 pr-7 w-full"
-            onClick={(e) => {
-              e.preventDefault();
-              setIsActive(!isActive);
-            }}
+            {...getToggleProps({
+              onClick: () => setExpanded((prevExpanded) => !prevExpanded),
+            })}
           >
             <div
               class=" float-left 
@@ -26,25 +25,25 @@ const Accordion = ({ title, content }) => {
             ></div>
 
             <div className="font-body text-2x text-justify ">
-              <p className="w-9/12 float-left ml-4 text-gray-500 font-body sm:text-lg lg:text-xl xl:text-2x"> {title}</p>
+              <p className="w-9/12 float-left ml-4 text-gray-500 font-body sm:text-lg lg:text-xl xl:text-2x">
+                {" "}
+                {title}
+              </p>
               <p className="text-4xl font-thin float-right text-gray-500 font-body text-right cursor-pointer">
                 {" "}
-                {isActive ? "-" : "+"}
+                {isExpanded ? "-" : "+"}
               </p>
             </div>
           </div>
-          {/* {isActive && (
-            <div className="float-left w-full pr- border-gray-500 shadow m-2 font-body text-lg font-normal p-4 text-justify ">
+          <section {...getCollapseProps()}>
+            <div
+              id="example-collapse-text"
+              style={{ borderRadius: "10px" }}
+              className="float-left w-full pr- border-gray-500 shadow m-2 font-body text-lg font-normal p-4 text-justify "
+            >
               {content}
             </div>
-          )} */}
-           <Collapse in={isActive}>
-           <div 
-              style={{borderRadius:"10px"}}
-              className="float-left w-full pr- border-gray-500 shadow m-2 font-body text-lg font-normal p-4 text-justify ">
-              {content}
-            </div>
-          </Collapse>
+          </section>
         </Col>
       </Row>
     </div>
--- src/components/Accordion/HomeAccordion.js
+++ src/components/Accordion/HomeAccordion.js
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
 import { Row, Col } from "react-bootstrap";
-import Collapse from "react-bootstrap/Collapse";
+import useCollapse from "react-collapsed";
 
 const HomeAccordion = ({ title, content }) => {
-  const [isActive, setIsActive] = useState(false);
-
+  const [isExpanded, setExpanded] = useState(false);
+  const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded });
   return (
     <div className="accordion-item">
       <Row>
         <Col md="12" lg="12">
           <div
             className=""
-            onClick={(e) => {
-              e.preventDefault();
-              setIsActive(!isActive);
-            }}
+            {...getToggleProps({
+              onClick: () => setExpanded((prevExpanded) => !prevExpanded),
+            })}
           >
             <div
               className={`font-body text-2x text-justify   flex mb-3 ${
-                isActive ? "border-none" : "border-b-2 border-gray-500"
+                isExpanded ? "border-none" : "border-b-2 border-gray-500"
               }`}
             >
               <p className="  text-gray-500 font-body sm:text-lg lg:text-xl xl:text-2x">
@@ -27,29 +26,21 @@ const HomeAccordion = ({ title, content }) => {
               </p>
               <p className="ml-auto mb-0 text-4xl font-thin cursor-pointer   text-gray-500 font-body text-right">
                 {" "}
-                {isActive ? "-" : "+"}
+                {isExpanded ? "-" : "+"}
               </p>
             </div>
           </div>
-
-          {/* {isActive && (
-               <div
-          className="border-b-2 border-gray-500 float-left w-full pb-3 mb-3  font-body text-lg font-normal  text-justify ">
-              {content} 
-            </div>
-            
-          )} */}
-
-          <Collapse in={isActive} timeout={500}>
+          <section {...getCollapseProps()}>
             <div
               id="example-collapse-text"
               className="border-b-2 border-gray-500 float-left w-full pb-3 mb-3  font-body text-lg font-normal  text-justify "
             >
               {content}
             </div>
-          </Collapse>
+          </section>
         </Col>
       </Row>
+      <div></div>
     </div>
   );
 };
