3b34d3fa9e12f10423c2636c691e0519bb230b41
Muzammal Rahim <creative.joomdev@gmail.com>
2021-07-30 09:46:34 +0500

    chages

--- src/components/Banners/InvestorsBanner.js
+++ src/components/Banners/InvestorsBanner.js
@@ -40,31 +40,7 @@ class InvestorsBanner extends Component {
 
                     </div>
                 </div>
-                <div className="">
-                    <div className="sm:right-inner pb-5  xs:right-inner inner bg-local">
-                        {/* <div className="sm:inner xs:inner md:inner-pt-5 lg:inner">
-                            <h2 className="text-green-600 text-xl md:text-4xl lg:text-6xl font-normal le">
-                                {t('banner.pay')}
-                                <br/>
-                                {t('banner.rent')}
-                                <br/>
-                                {t('banner.monthly')}
-                            </h2>
-                            <p className="text-gray-500 pt-3 pb-3 text-lg md:text-2lx leading-7">
-                                {t('banner.banner-details-1')}
-                                <br/>
-                                {t('banner.banner-details-2')}
-                            </p>
-                        
-                            <button className="bg-green-600 rounded-full px-4 py-2 text-lg text-white font-bold font-body header-btn">
-                                {t('banner.readmore')}
-                            </button>
-                        </div> */}
-                        {/* <img className="invest-banner-img z-10 relative top-20" src={Bannerimg} alt="img"/> */}
-                        
-                    </div>
-                    
-                </div>
+                
             </div> 
         </div>
         )
