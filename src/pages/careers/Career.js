b0357f90b033a643af7bee6b29f9bb1234fdae30
Muzammal Rahim <creative.joomdev@gmail.com>
2021-08-05 10:31:34 +0500

    responsive

--- src/pages/careers/Career.js
+++ src/pages/careers/Career.js
@@ -6,37 +6,52 @@ import CareerBanner from "../../components/Banners/CareerBanner";
 import phone from "../../assets/images/phone.png";
 import brand from "../../assets/images/brand.png";
 import CareerBannerImg from "../../assets/images/CareerBanner.png";
+// import CBImg from "../../assets/images/career-banner.jpg";
+import CBImg from "../../assets/images/career-banner2.jpg";
+
 
 function Career() {
   return (
     <div className="career">
+      {/* <div
+        className="relative c-h-screen bg-no-repeat"
+        style={{ backgroundImage: `url(${CBImg})` }}
+      > */}
       <div
-        className="relative min-h-screen bg-no-repeat"
-        style={{ backgroundImage: `url(${CareerBannerImg})` }}
-      >
+        className="relative c-h-screen bg-no-repeat">
+         
         {/* <div> */}
+        <div className="">
         <Header />
+        </div>
+        
         {/* <img className="min-w-full -mt-20" src={CareerBannerImg} alt="" /> */}
-        <div className="absolute xl:top-64 lg:top-52 top-64 right-0">
-          <div className="w-4/6 lg:float-right xl:float-right">
-            {/* <h1 className=" career__head mb-8">Welcome to Our Careers Page</h1> */}
-            <h1
-              className="xl:text-6xl lg:text-6xl md:text-5x 
- font-normal text-green-900 font-body 
-  xs_Title lineHeight mb-6"
-            >
-              Welcome to Our Careers Page
-            </h1>
-            {/* <h6 className="career__text ">Join the Prop-Tech revolution</h6> */}
-            <h6
-              className="xl:text-3xl lg:text-3xl md:text-xl 
- font-bold text-gray-500 font-body  
-  xs_heading lineHeightt "
-            >
-              Join the Prop-Tech revolution
-            </h6>
+        <div className="relative caree-b-main">
+          <div className="cm-bg"></div>
+          <img className="cb-img w-full -mt-20" src={CBImg} alt=""/>
+          <div className="c-banner-text">
+            <div className="first-side"></div>
+            <div className="sec-side w-8/12">
+              {/* <h1 className=" career__head mb-8">Welcome to Our Careers Page</h1> */}
+              <h1
+                className="xl:text-5xl lg:text-5xl md:text-5x 
+                            font-normal text-green-900 font-body 
+                          xs_Title lineHeight mb-6"
+              >
+                Welcome to Our Careers Page
+              </h1>
+              {/* <h6 className="career__text ">Join the Prop-Tech revolution</h6> */}
+              <h6
+                className="xl:text-3xl lg:text-3xl md:text-xl 
+  font-bold text-gray-500 font-body  
+    xs_heading lineHeightt "
+              >
+                Join the Prop-Tech revolution
+              </h6>
+            </div>
           </div>
         </div>
+        
       </div>
 
       <CareerBanner />
@@ -49,28 +64,29 @@ function Career() {
         </h1>
         <div className="row">
           <div className="col-md-6 text-left mt-8 property__techText xs_para_size">
-            We consider ourselves a property technology company... <br />
+            <p>We consider ourselves a property technology company... </p>
+            <p>Why is that so important I hear you ask?</p>
+            <p>Simple...</p>
             <br />
-            Why is that so important I hear you ask? <br />
-            <br />
-            Simple... <br />
-            <br />
-            It is well known that around the world technology is developing very
-            fast. However, we believe there is a lag in Property Technology. In
-            many "developed" nations property is very years old, and expensive
-            to change. 
-            <br />
-            <br />
-            Our aim is to be part of the property technology revolution and
-            truly change the way people live/rent their homes. Join us.. <br />
-            <br />
-            This is only the begining and we want to use this as a platform to
-            branch out into other fields and truly use technology to shake up
-            the industry as it needs. <br />
-            <br />
-            What are you waiting for.... Fill out an application below. 
-            <br />
-            <br />
+            <p>
+              It is well known that around the world technology is developing very
+              fast. However, we believe there is a lag in Property Technology. In
+              many "developed" nations property is very years old, and expensive
+              to change. 
+            </p>
+
+            <p>
+              Our aim is to be part of the property technology revolution and
+              truly change the way people live/rent their homes. Join us..
+            </p>
+            <p>
+              This is only the begining and we want to use this as a platform to
+              branch out into other fields and truly use technology to shake up
+              the industry as it needs.
+            </p>
+            <p>
+              What are you waiting for.... Fill out an application below. 
+            </p>
           </div>
           <div className="col-md-6 right">
             <img src={phone} alt="" className="phone" />
@@ -88,7 +104,7 @@ function Career() {
           alt="RightBg"
         />
         <div className="absolute  xl:left-44 lg:left-32 xl:bottom-80 lg:bottom-44 left-8 bottom-6 xs_left_bottom ">
-          <div className="mt-4">
+          <div className="">
             <div className=" ">
               <div className=" w-4/12 text-left">
                 <h2
